ad362a7c6a665d6122512afdf71b95ebc05ee056
Starostina Helen <starostina.lena05@gmail.com>
2024-12-19 01:55:47 +0300

    fixed bug with 500 error after double form sending

--- main.py
+++ main.py
@@ -50,6 +50,7 @@ def start_page():
         message = 'Физика любит числа :)'
         validation_error = True
     if answer_input_form.validate_on_submit():
+        print(graph_data)
         if not check_similar(answer_input_form.sand_speed.data, answer_input_form.generated_sand_speed.data):
             message += 'Введен неверный \u03BC' + '<br>'
             validation_error = True
@@ -70,14 +71,25 @@ def start_page():
             message = 'Правильный ответ!'
             correct_answer = True
 
-    return render_template(
-        'index.html',
-        answer_input_form=answer_input_form,
-        message=message,
-        validation_error=validation_error,
-        correct_answer=correct_answer,
-        graph_data=graph_data  # Передаем данные для графиков
-    )
+    try:
+        return render_template(
+            'index.html',
+            answer_input_form=answer_input_form,
+            message=message,
+            validation_error=validation_error,
+            correct_answer=correct_answer,
+            graph_data=graph_data  # Передаем данные для графиков
+        )
+    except NameError:
+        return render_template(
+            'index.html',
+            answer_input_form=answer_input_form,
+            message=message,
+            validation_error=validation_error,
+            correct_answer=correct_answer,
+            graph_data=None  # Передаем данные для графиков
+        )
+
 
 
 if __name__ == '__main__':
